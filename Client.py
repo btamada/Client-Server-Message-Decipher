c52b0618af94542d995c6732dd5e2da0bb4d6fb1
Bryan Tamada <btamada@users.noreply.github.com>
2015-10-19 23:08:08 -0700

    Added AES encryption to the Client and Server files

--- Client.py
+++ Client.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python
 
-__author__ = 'User1'
+__author__ = 'Bryan Tamada'
 
+from Crypto.Cipher import AES
 import socket
 
-top.mainloop()
+def encrypt(secret):
+    encryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+    cipher_text = encryption_suite.encrypt(secret)
+    return cipher_text
 
-host = socket.gethostname()
+def decrypt(cipher):
+    decryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+    plain_text = decryption_suite.decrypt(cipher)
+    return plain_text
 
-# the port the server is listening on
-port = 9999
+# create the socket
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connect to the server
+mysocket.connect((socket.gethostname(), 12345))
 
-s.connect((host, port))
+# send the message to the server
+mysocket.sendall(encrypt("This my secretss"))
 
-s.sendall(b'Hello, world')
+# receive and store the response from the server in 1024 bit chunks
+server_response = mysocket.recv(1024)
 
-data = s.recv(1024)
+# close the socket
+mysocket.close()
 
-s.close()
-
-print('Received', repr(data))
+# print out the server response message
+print('Received response from server %s' % decrypt(server_response).decode('ascii'))
--- Server.py
+++ Server.py
@@ -1,22 +1,43 @@
-__author__ = 'User1'
+#!/usr/bin/python
 
+__author__ = 'Bryan Tamada'
+
+from Crypto.Cipher import AES
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def encrypt(secret):
+    encryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+    cipher_text = encryption_suite.encrypt(secret)
+    return cipher_text
 
-s.bind((socket.gethostname(), 9999))
+def decrypt(cipher):
+    decryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+    plain_text = decryption_suite.decrypt(cipher)
+    return plain_text
 
-s.listen(5)
+# create the socket
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-conn, addr = s.accept()
+# bring the host name of the server and the port to listen to
+mysocket.bind((socket.gethostname(), 12345))
 
-print('Establishing a connection on (host/IP, port) - ', addr)
+# listen to the port and accept 1 request from the client
+mysocket.listen(1)
 
-while True:
-    data = conn.recv(1024)
-    if not data: break
+# accept() returns the socket object and client socket address
+conn, addr = mysocket.accept()
 
-    conn.sendall(data)
+print('Connected to', addr)
+
+# receive the secret message from the client in 1024 blocks
+client_msg = conn.recv(1024)
+
+plain_text = decrypt(client_msg)
+
+print('The server received the message %s' % plain_text.decode('ascii'))
+
+# send the message back to the client
+conn.sendall(encrypt(plain_text))
 
 # close the socket
 conn.close()

9b285650931b2b20cb4dd7c60438cd22a8637327
Bryan Tamada <btamada@users.noreply.github.com>
2015-10-26 23:33:07 -0700

    Updated the Decipher Algorithm

--- Server.py
+++ Server.py
@@ -2,7 +2,7 @@
 
 import socket
 from Crypto.Cipher import AES
-from marisa_trie import RecordTrie
+from marisa_trie import Trie
 
 def encrypt(secret):
     encryption_suite = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
@@ -15,45 +15,43 @@ def decrypt(cipher):
     return plain_text
 
 def decipher(plain_text):
-    # insert algorithm to decipher the plain_text
-    trie[str(plain_text, 'utf-8')]
 
-    # return the resulting text
-    decipher_text = "this is the result"
+    decipher_text = ""
+
+    # insert algorithm to decipher the plain_text
+    text_list = plain_text.split()
+
+    for text in text_list:
+        if(text in trie):
+            decipher_text += str(trie.prefixes(text))
+
+    # Pad the decipher text to make it a multiple of 16 in length
+    if(len(decipher_text) % 16 != 0):
+        while(len(decipher_text) % 16 != 0):
+            decipher_text += " "
+
     return decipher_text
 
 # Create the Trie Data Structure
-keys = [u'a', u'b', u'c', u'd', u'e', u'f']
-values = ["Allison", "Bryan", "Cindy", "Darryl", "Edward", "Frank"]
-fmt = "<HH"
-trie = RecordTrie.RecordTrie(fmt, zip(keys, values))
+trie = Trie([u'this', u'is', u'a', u'test', u'testicular', u'apple', u'istanbul',
+             u'thisser', u'secret', u'message', u'massage'])
 
-
-# create the socket
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# bring the host name of the server and the port to listen to
 mysocket.bind((socket.gethostname(), 12345))
 
-# listen to the port and accept 1 request from the client
 mysocket.listen(1)
 
-# accept() returns the socket object and client socket address
 conn, addr = mysocket.accept()
 
 print('Connected to', addr)
 
-# receive the secret message from the client in 1024 blocks
 client_msg = conn.recv(1024)
 
-# decrypt the client message
 plain_text = decrypt(client_msg)
 
-#decipher the client message
-decipher_text = decipher(plain_text)
+decipher_text = decipher(str(plain_text))
 
-# send the message back to the client
 conn.sendall(encrypt(decipher_text))
 
-# close the socket
 conn.close()
